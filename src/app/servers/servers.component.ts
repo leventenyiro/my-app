0bdf43bab270c33a868021ca2d65fd5be340fa61
Levente Nyiro <lnyiro@berriesand.co>
2022-01-28 09:03:50 +0100

    ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent {
     serverCreationStatus = 'No server was created';
     serverName = 'Testserver';
     serverCreated = false;
+    servers = ['Testserver', 'Testserver 2'];
     
     constructor() {
         setTimeout(() => {
@@ -23,6 +24,7 @@ export class ServersComponent {
 
     onCreateServer() {
         this.serverCreated = true;
+        this.servers.push(this.serverName);
         this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
     }
 
